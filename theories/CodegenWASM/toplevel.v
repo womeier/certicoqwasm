02b71dcf464b8e6d2882b3991908536921e3e4e2
Wolfgang Meier <womeier@posteo.de>
2023-02-25 22:02:46 +0100

    basic allocation of constructor arguments in linear memory, cleanup

--- theories/CodegenWASM/toplevel.v
+++ theories/CodegenWASM/toplevel.v
@@ -34,8 +34,8 @@ Inductive var :=
     
 Inductive wasm_instr :=
   | WI_unreachable : wasm_instr                               (* trap unconditionally *)
-  | WI_noop : wasm_instr                                      (* do nothing *)
-  | WI_noop_comment : string -> wasm_instr                    (* do nothing *)
+  | WI_nop : wasm_instr                                       (* do nothing *)
+  | WI_comment : string -> wasm_instr                         (* do nothing *)
   | WI_block : list wasm_instr -> wasm_instr                  (* execute in sequence, for now just a list of instructions without block nesting *)
   | WI_if : wasm_instr -> wasm_instr -> wasm_instr            (* conditional *)
   | WI_return : wasm_instr                                    (* break from function body *)
@@ -79,15 +79,14 @@ Definition var_show (v : var) :=
 
 
 Definition instr_list_show (l : list wasm_instr) (show : wasm_instr -> string) : string :=
-  (fold_left (fun _s i => _s ++ show i ++ nl) l "") ++ nl.
+  (fold_left (fun _s i => _s ++ show i) l "").
 
 (* TODO: typeclass show *)
-(* TODO readd environment variables and stuff *)
 Fixpoint instr_show (e : wasm_instr) : string := 
   (match e with
   | WI_unreachable => "unreachable"
-  | WI_noop  => "nop"
-  | WI_noop_comment s => "nop ;; " ++ s
+  | WI_nop  => "nop"
+  | WI_comment s => nl ++ ";; " ++ s
   | WI_block instructions => instr_list_show instructions instr_show
   | WI_return => "return"
   | WI_local_get x => "local.get " ++ var_show x
@@ -140,6 +139,8 @@ Definition wasm_module_show (m : wasm_module) : string :=
     global_vars_show "global" m.(global_vars) ++ nl ++
     (fold_left (fun s f => s ++ nl ++ function_show f) m.(functions) "") ++ nl ++ ")".
 
+(********************** BEGIN TRANSLATION *******************************)
+
 (*
 (* Expressions [exp] of the LambdaANF language. *)
 Inductive exp : Type :=
@@ -158,6 +159,9 @@ with fundefs : Type :=
 
 *)
 
+Definition constr_alloc_function_prefix := "$alloc_constr_".
+Definition global_mem_ptr := Generic "$ptr".
+
 Definition translate_var (nenv : name_env) (v : cps.var) : var :=
   Generic ("$" ++ show_tree (show_var nenv v)).
 
@@ -169,21 +173,21 @@ Definition translate_var (nenv : name_env) (v : cps.var) : var :=
 Fixpoint translate_exp (nenv : name_env) (cenv : ctor_env) (ftag_flag : bool) (e : exp) : error wasm_instr :=
    match e with
    | Efun fundefs e => Err "unexpected nested function definition"
-   | Econstr x c ys e => Ret (WI_noop_comment ("econstr: " ++ show_tree (show_var nenv x)))
+   | Econstr x c ys e => Ret (WI_comment ("econstr: " ++ show_tree (show_var nenv x)))
    | Ecase x arms => Ret (
      WI_block (* TODO: choose dynamically *)
-              [ WI_noop_comment ("ecase: " ++ show_tree (show_var nenv x))
-              ; WI_noop_comment "load <mem address>"
-              ; WI_noop_comment "compare with possible constructors"
+              [ WI_comment ("ecase: " ++ show_tree (show_var nenv x))
+              ; WI_comment "load <mem address>"
+              ; WI_comment "compare with possible constrs"
               ; WI_local_get (translate_var nenv x)
               ; WI_return
               ])
 
-   | Eproj x tg n y e => Ret (WI_noop_comment "proj")
-   | Eletapp x f ft ys e => Ret (WI_noop_comment "letapp")
-   | Eapp x ft ys => Ret (WI_noop_comment "app")
-   | Eprim_val x p e => Ret (WI_noop_comment "prim val")
-   | Eprim x p ys e => Ret (WI_noop_comment "prim")
+   | Eproj x tg n y e => Ret (WI_comment "proj")
+   | Eletapp x f ft ys e => Ret (WI_comment "letapp")
+   | Eapp x ft ys => Ret (WI_comment "app")
+   | Eprim_val x p e => Ret (WI_comment "prim val")
+   | Eprim x p ys e => Ret (WI_comment "prim")
    | Ehalt e => Ret WI_return
    end.
 
@@ -208,37 +212,100 @@ Definition translate_function (nenv : name_env) (cenv : ctor_env) (ftag_flag : b
                             (name : cps.var) (args : list cps.var) (body : exp): error wasm_function :=
   bodyRes <- translate_exp nenv cenv ftag_flag body ;;
   Ret
-  {| name := translate_var nenv name;
-     export := true;
-     args := map (fun p => (translate_var nenv p, I64)) args;
-     ret_type := I64;
-     locals := map (fun p => (p, I64)) (collect_local_variables nenv body);
-     body := bodyRes |}.
+  {| name := translate_var nenv name
+   ; export := true
+   ; args := map (fun p => (translate_var nenv p, I64)) args
+   ; ret_type := I64
+   ; locals := map (fun p => (p, I64)) (collect_local_variables nenv body)
+   ; body := bodyRes
+   |}.
 
-Fixpoint collect_constructor_tags (e : exp) {struct e} : list ctor_tag :=
+Fixpoint collect_constr_tags (e : exp) {struct e} : list ctor_tag :=
+(* TODO: make unique, use set *)
   match e with
-  | Efun _ e' => collect_constructor_tags e'
-  | Econstr _ t _ e' => t :: collect_constructor_tags e'
-  | Ecase _ arms => List.concat (map (fun a => collect_constructor_tags (snd a)) arms)
-  | Eproj _ _ _ _ e' => collect_constructor_tags e'
-  | Eletapp _ _ _ _ e' => collect_constructor_tags e'
-  | Eprim _ _ _ e' => collect_constructor_tags e'
-  | Eprim_val _ _ e' => collect_constructor_tags e'
+  | Efun _ e' => collect_constr_tags e'
+  | Econstr _ t _ e' => t :: collect_constr_tags e'
+  | Ecase _ arms => List.concat (map (fun a => collect_constr_tags (snd a)) arms)
+  | Eproj _ _ _ _ e' => collect_constr_tags e'
+  | Eletapp _ _ _ _ e' => collect_constr_tags e'
+  | Eprim _ _ _ e' => collect_constr_tags e'
+  | Eprim_val _ _ e' => collect_constr_tags e'
   | Eapp _ _ _ => []
   | Ehalt _ => []
   end.
 
-Definition generate_constructor_allocator_function (cenv : ctor_env) (c : ctor_tag) : wasm_function :=
-  {| name := Generic ("$alloc_constr_" ++ show_tree (show_con cenv c));
-     export := true;
-     args := [];
-     ret_type := I32;
-     locals := [];
-     body := WI_block [
-        WI_const (Generic "42") I32;
-        WI_return
-     ]|}.
-(* generates for constructor e a function that takes the arguments
+(* cenv : Map[ctor_tag -> rec]:
+
+{ ctor_name     : name    (* the name of the constructor *)
+; ctor_ind_name : name    (* the name of its inductive type *)
+; ctor_ind_tag  : ind_tag (* ind_tag of corresponding inductive type *)
+; ctor_arity    : N       (* the arity of the constructor *)
+; ctor_ordinal  : N       (* the [ctor_tag]s ordinal in inductive defn starting at zero *)
+}.
+
+*)
+
+(* generates argument list for constructor with arity n*)
+Fixpoint arg_list (n : nat) : list (var * type) :=
+  match n with
+  | 0 => []
+  | S n' => arg_list n' ++ [(Generic ("$arg" ++ string_of_nat n'), I32)]
+  end.
+
+Definition generate_constr_alloc_function (cenv : ctor_env) (c : ctor_tag) : wasm_function :=
+  let ctor_id := show_tree (show_con cenv c) in  (*TODO: translate to I32, fail if e.g. too large*)
+  let return_var := Generic "$ret_pointer" in
+(*  let info :=
+    (match M.get c cenv with
+     | Some {| ctor_name := nNamed name
+             ; ctor_ind_name := nNamed ind_name
+             |} => "ind type: " ++ ind_name ++ ", constr name: " ++ name
+     | _ => "error: didn't find information of constructor"
+     end) in *)
+
+  let args := arg_list
+    (match M.get c cenv with 
+     | Some {| ctor_arity := n |} => N.to_nat n
+     | _ => 42 (*TODO: handle error*)
+     end) in
+
+  {| name := Generic (constr_alloc_function_prefix ++ ctor_id)
+   ; export := true
+   ; args := args
+   ; ret_type := I32
+   ; locals := [(return_var, I32)]
+   ; body := WI_block
+    ([ WI_comment "save ret pointer"
+     ; WI_global_get global_mem_ptr
+     ; WI_local_set return_var
+
+     ; WI_comment "copy const id"
+     ; WI_global_get global_mem_ptr
+     ; WI_const (Generic (ctor_id)) I32
+     ; WI_store I32
+     ; WI_global_get global_mem_ptr
+     ; WI_const (Generic "4") I32
+     ; WI_add I32
+     ; WI_global_set global_mem_ptr
+     ] ++ (* store argument pointers in memory *)
+       concat (map (fun arg =>
+         [ WI_comment ("storing " ++ var_show (fst arg) ++ " in memory")
+         ; WI_global_get global_mem_ptr
+         ; WI_local_get (fst arg)
+         ; WI_store I32
+         ; WI_global_get global_mem_ptr
+         ; WI_const (Generic "4") I32
+         ; WI_add I32
+         ; WI_global_set global_mem_ptr
+         ]
+       ) args) 
+     ++
+     [ WI_comment "return pointer to beginning of memory segment"
+     ; WI_local_get return_var
+     ; WI_return
+     ])
+   |}.
+(* generates for constr e a function that takes the arguments
   and allocates a record in the linear memory
 *)
 
@@ -259,15 +326,15 @@ Definition LambdaANF_to_WASM (nenv : name_env) (cenv : ctor_env) (ftag_flag : bo
     | _ => ([], e)
   end in
 
-  let constr_allocator_functions :=
-    map (generate_constructor_allocator_function cenv) (collect_constructor_tags mainExpr) in
+  let constr_alloc_functions :=
+    map (generate_constr_alloc_function cenv) (collect_constr_tags mainExpr) in
 
   mainInstr <- translate_exp nenv cenv ftag_flag mainExpr ;;
-  Ret {| memory := Generic "100";
-         functions := constr_allocator_functions ++ fns;
-         global_vars := [(Generic "$ptr", I64, Generic "0")];
-         start := mainInstr |}.
-
+  Ret {| memory := Generic "100"
+       ; functions := constr_alloc_functions ++ fns
+       ; global_vars := [(Generic "$ptr", I64, Generic "0")]
+       ; start := mainInstr
+       |}.
 
 Definition LambdaANF_to_WASM_Wrapper (prims : list (kername * string * bool * nat * positive)) (args : nat) (t : toplevel.LambdaANF_FullTerm) : error wasm_module * string :=
   let '(_, pr_env, cenv, ctag, itag, nenv, fenv, _, prog) := t in
