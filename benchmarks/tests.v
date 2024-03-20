0ef3a8830807b76adafcb6ad64198dcd80fa6ba6
Wolfgang Meier <womeier@posteo.de>
2024-03-20 23:44:40 +0100

    readd color benchmark

--- benchmarks/tests.v
+++ benchmarks/tests.v
@@ -79,13 +79,15 @@ Definition sha := sha256.SHA_256 (sha256.str_to_bytes test).
 
 Definition sha_fast := sha256.SHA_256' (sha256.str_to_bytes test).
 
-(* Eval compute in "Compiling demo1".*)
+Eval compute in "Compiling demo1".
+CertiCoq Generate WASM -cps -time -debug demo1.
 
 (* CertiCoq Generate WASM -cps -debug demo1. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo1. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo1. *)
 
-(* Eval compute in "Compiling demo2". *)
+Eval compute in "Compiling demo2".
+CertiCoq Generate WASM -cps -time -debug demo2.
 
 (* CertiCoq Generate WASM -cps -debug demo2. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo2. *)
@@ -99,7 +101,8 @@ CertiCoq Generate WASM -cps -debug demo3.
 (* CertiCoq Compile -cps -ext "_cps_opt" demo3. *)
 *)
 
-(* Eval compute in "Compiling list_sum". *)
+Eval compute in "Compiling list_sum".
+CertiCoq Generate WASM -cps -time -debug list_sum.
 
 (* CertiCoq Generate WASM -debug list_sum. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" list_sum. *)
@@ -137,8 +140,9 @@ CertiCoq Compile -args 1000 -config 9 -O 1 -ext "_opt_ll" lazy_factorial. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo1. *)
 
 
-(* Eval compute in "Compiling color". *)
+Eval compute in "Compiling color".
 
+CertiCoq Generate WASM -cps -time -debug color.
 (* CertiCoq Generate WASM -cps -time -debug color. *)
 (* CertiCoq Compile -O 0 -time -cps -ext "_cps" color. *)
 (* CertiCoq Compile -time -cps -ext "_cps_opt" color. *)
