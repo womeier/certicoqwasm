ab13533401f27b698438974ff481ae24d42a0a10
Wolfgang Meier <womeier@posteo.de>
2024-04-06 00:43:49 +0200

    add more benchmarks

--- benchmarks/tests.v
+++ benchmarks/tests.v
@@ -1,9 +1,11 @@
-Require Import Arith List String.
+Require Import Arith List String Uint63 BinNat.
 Require Import CertiCoq.Benchmarks.lib.vs.
 Require Import CertiCoq.Benchmarks.lib.Binom.
 Require Import CertiCoq.Benchmarks.lib.Color.
 Require Import CertiCoq.Benchmarks.lib.sha256.
 Require Import CertiCoq.Benchmarks.lib.coind.
+Require Import CertiCoq.Benchmarks.lib.BersteinYangTermination.
+Require Import CertiCoq.Benchmarks.lib.stack_machine.
 From MetaCoq.Utils Require Import bytestring MCString.
 From CertiCoq.Plugin Require Import CertiCoq.
 
@@ -16,7 +18,6 @@ Import VeriStar.
 
 CertiCoq -help.
 
-(* CertiCoq Generate Glue -file "basics" [ nat, list, bool ]. *)
 
 (* Demo 1 *)
 
@@ -39,6 +40,9 @@ Definition demo3 := andb.
 (* List sum *)
 
 Definition list_sum := List.fold_left plus (List.repeat 1 100) 0.
+
+(* Definition list_sum_primitive :=
+List.fold_left Uint63.add (List.repeat 1%uint63 100) 0%uint63. *)
 
 (* Veristar *)
 
@@ -79,17 +83,60 @@ Definition sha := sha256.SHA_256 (sha256.str_to_bytes test).
 
 Definition sha_fast := sha256.SHA_256' (sha256.str_to_bytes test).
 
-Eval compute in "Compiling demo1".
 
+(*******************************************************************)
+(* from https://github.com/AU-COBRA/coq-rust-extraction/blob/master/tests/theories/InternalFix.v *)
+
+Fixpoint ack (n m : nat) : nat :=
+  match n with
+  | O => S m
+  | S p => let fix ackn (m : nat) :=
+            match m with
+            | O => ack p 1
+            | S q => ack p (ackn q)
+            end
+          in ackn m
+  end.
+Definition ack_3_9 := ack 3 9.
+
+Fixpoint even n :=
+  match n with
+  | O => true
+  | S m => odd m
+  end
+  with odd n :=
+    match n with
+    | O => false
+    | S k => even k
+    end.
+Definition even_10000 := even 10000.
+
+Definition bernstein_yang := W 10.
+
+Eval compute in "Compiling ack".
+CertiCoq Generate WASM -debug ack_3_9.
+
+Eval compute in "Compiling even_10000".
+CertiCoq Generate WASM -debug even_10000.
+
+Eval compute in "Bernstein yang termination".
+CertiCoq Generate WASM -debug bernstein_yang.
+(* bernstein_yang: compilation fine, runs for quite long *)
+
+(*******************************************************************)
+Eval compute in "Compiling demo1".
 CertiCoq Generate WASM -debug demo1.
+
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo1. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo1. *)
+(* CertiCoq Generate Glue -file "glue_demo1" [ list, bool ]. *)
 
 Eval compute in "Compiling demo2".
-
 CertiCoq Generate WASM -debug demo2.
+
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo2. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo2. *)
+(* CertiCoq Generate Glue -file "glue_demo2" [ list, bool ]. *)
 
 (*
 Eval compute in "Compiling demo3".
@@ -98,35 +145,39 @@ CertiCoq Generate WASM -cps -debug demo3.
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo3. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo3. *)
 *)
+(* CertiCoq Generate Glue -file "glue_demo3" [ list, bool ]. *)
 
 Eval compute in "Compiling list_sum".
-
 CertiCoq Generate WASM -debug list_sum.
 (* CertiCoq Compile -O 0 -cps -ext "_cps" list_sum. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" list_sum. *)
+(* CertiCoq Generate Glue -file "glue_list_sum" [ nat ]. *)
 
+(* Eval compute in "Compiling list_sum_primitive".
+CertiCoq Generate WASM -debug list_sum_primitive. *)
 
 Eval compute in "Compiling vs_easy".
-
-(* CertiCoq Generate WASM -cps -time -debug vs_easy. *)
 CertiCoq Generate WASM -time -debug vs_easy.
+(* CertiCoq Generate WASM -cps -time -debug vs_easy. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" -time_anf vs_easy. *)
 (* CertiCoq Compile -time -cps -ext "_cps_opt" vs_easy. *)
+(* CertiCoq Generate Glue -file "glue_vs_easy" [ list, bool, vs.space_atom, vs.clause ]. *)
 
 Eval compute in "Compiling vs_hard".
-
-(* CertiCoq Generate WASM -cps -time -debug vs_hard. *)
 CertiCoq Generate WASM -time -debug vs_hard.
+(* CertiCoq Generate WASM -cps -time -debug vs_hard. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" vs_hard. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" vs_hard. *)
+(* CertiCoq Generate Glue -file "glue_vs_hard" [ list, bool ]. *)
 
 
 Eval compute in "Compiling binom".
-
-(* CertiCoq Generate WASM -cps -time -debug binom. *)
 CertiCoq Generate WASM -time -debug binom.
+(* CertiCoq Show IR -file "binom" binom. *)
+(* CertiCoq Generate WASM -cps -time -debug binom. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" binom. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" binom. *)
+(* CertiCoq Generate Glue -file "glue_binom" [ nat ]. *)
 
 (* Eval compute in "Compiling lazy factorial". *)
 
@@ -135,12 +186,14 @@ CertiCoq Compile -ext "_opt" lazy_factorial.
 CertiCoq Compile -args 1000 -config 9 -O 1 -ext "_opt_ll" lazy_factorial. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" demo1. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" demo1. *)
-
+(* CertiCoq Generate Glue -file "glue_lazy_factorial" [ ]. *)
 
 Eval compute in "Compiling color".
 CertiCoq Generate WASM -time -debug color.
+
 (* CertiCoq Compile -O 0 -time -cps -ext "_cps" color. *)
 (* CertiCoq Compile -time -cps -ext "_cps_opt" color. *)
+(* CertiCoq Generate Glue -file "glue_color" [ prod, Z ]. *)
 
 (* Don't compile slow sha *)
 (* Eval compute in "Compiling sha". *)
@@ -149,10 +202,42 @@ CertiCoq Generate WASM -time -debug color.
 (* CertiCoq Compile sha. *)
 (* CertiCoq Compile -O 1 -cps -ext "_cps_opt" sha. *)
 (* CertiCoq Compile -O 1 -ext "_opt" sha. *)
+(* CertiCoq Generate Glue -file "glue_sha" [ ]. *)
 
 Eval compute in "Compiling sha_fast".
-
-(* CertiCoq Generate WASM -cps -time -debug sha_fast. *)
 CertiCoq Generate WASM -time -debug sha_fast.
+(* CertiCoq Generate WASM -cps -time -debug sha_fast. *)
 (* CertiCoq Compile -O 0 -cps -ext "_cps" sha_fast. *)
 (* CertiCoq Compile -cps -ext "_cps_opt" sha_fast. *)
+(* CertiCoq Generate Glue -file "glue_sha_fast" [ ]. *)
+
+(* Eval compute in "Compiling parse_wasm_module". *)
+(* CertiCoq Generate WASM -time -debug test_module. *)
+
+Definition sm_gauss_nat :=
+  let n := 1000 in
+  match (s_execute' (gauss_sum_sintrs_nat n)) with
+  | [ n' ] => Some (n' - (n * (n + 1))/2)
+  | _ => None
+  end.
+
+Definition sm_gauss_N :=
+  let n := 1000%N in
+  match (s_execute' (gauss_sum_sintrs_N n)) with
+  | [ n' ] => Some (n' - (n * (n + 1))/2)%N
+  | _ => None
+  end.
+
+(* Definition sm_gauss_PrimInt := *)
+(*   let n := 1000%uint63 in *)
+(*   match (s_execute' (gauss_sum_sintrs_PrimInt n)) with *)
+(*   | [ n' ] => Some (n' - (n * (n + 1))/2)%uint63 *)
+(*   | _ => None *)
+(*   end. *)
+
+CertiCoq Generate WASM -debug sm_gauss_nat.
+
+CertiCoq Generate WASM -debug sm_gauss_N.
+
+(* Not supported yet *)
+(* CertiCoq Generate WASM -debug sm_gauss_PrimInt. *)
