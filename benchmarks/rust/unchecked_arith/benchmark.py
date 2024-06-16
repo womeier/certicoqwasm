3339fb05e184ad94234a9669c868a83b57a42539
Wolfgang Meier <womeier@posteo.de>
2024-06-16 21:17:00 +0200

    unchecked_arith

---
+++ benchmarks/rust/unchecked_arith/benchmark.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import os
+import click
+import time
+import subprocess
+import pathlib
+
+CWD = os.path.abspath(os.path.dirname(__file__))
+os.chdir(CWD)
+
+measurements = ["time_startup", "time_main", "time_pp"]
+
+programs = [
+    "demo1",
+    "demo2",
+    "list_sum",
+    "binom",
+    "sha_fast",
+    "color",
+    "sm_gauss_N",
+    "sm_gauss_nat",
+    "even_10000",
+    "ack_3_9",
+    "bernstein_yang"
+]
+
+
+def single_run_rust(folder, program, verbose):
+    folder_path = os.path.join(os.path.join(CWD, folder), f"{program}-extracted")
+    os.chdir(folder_path)
+
+    start_time = time.time()
+    r = subprocess.run(
+        [
+            "cargo",
+            "run",
+        ],
+        capture_output=True,
+    )
+    end_time = time.time()
+
+    assert (
+        r.returncode == 0
+    ), f"Running {program} returned non-0 returncode"
+
+    if verbose:
+        print("STDOUT: " + r.stdout.decode("ascii"))
+        print("STDERR: " + r.stdout.decode("ascii"))
+
+    time_startup = round((end_time - start_time) * 1000)
+    return time_startup
+
+
+def org_table(tests, measure, results):
+    rows = [
+        [binary_version]
+        + ["N/A" if result.get(t) is None else f"{result[t][measure]}" for t in tests]
+        for (binary_version, result) in results.items()
+    ]
+
+    org_rows = ["| " + " | ".join(row) + " |\n" for row in rows]
+
+    org_string = (
+        ("|---|---" + "".join(["|---" for t in tests]) + "|\n")
+        + ("|   | " + " | ".join(tests) + " |\n")
+        + ("|---|---" + "".join(["|---" for t in tests]) + "|\n")
+        + (("|---|---" + "".join(["|---" for t in tests]) + "|\n").join(org_rows))
+        + ("|---|---" + "".join(["|---" for t in tests]) + "|\n")
+    )
+
+    print(org_string)
+
+
+@click.command()
+@click.option("--runs", type=int, help="Number of runs", default=10)
+@click.option("--binary-size", is_flag=True, help="Print binary size", default=False)
+@click.option("--folder", type=str, help="Folder to Wasm binaries", required=True)
+@click.option("--verbose", is_flag=True, help="Print debug information", default=False)
+@click.option(
+    "--print-org-table",
+    is_flag=True,
+    help="Print results as org mode table",
+    default=False,
+)
+def measure(runs, binary_size, folder, verbose, print_org_table):
+    assert runs > 0, "Expected at least one run."
+
+    f_name = pathlib.PurePath(folder).name
+    print(f"Running {f_name}, avg. of {runs} runs, rust binaries.")
+
+    for program in programs:
+        values = []
+        for run in range(runs):
+            res = single_run_rust(folder, program, verbose)
+
+            assert res is not None, "No value returned."
+            values.append(res)
+
+        result = dict()
+        for meas in measurements:
+            result[meas] = list()
+
+        for val in values:
+            for meas in measurements:
+                if meas not in ["time_startup", "time_main" ]:
+                    result["time_main"].append(int(val))
+
+        time_startup = 0
+        time_main = round(sum(result["time_main"]) / len(result["time_main"]))
+        time_pp = 0
+
+        # count spaces instead of using \t
+        max_program_len = max(map(len, programs))
+        program_pp = (max_program_len - len(program)) * " " + program
+
+        print(
+            f"{program_pp} : "
+            f"startup: {time_startup:>2}, main: {time_main:>3}, pp: {time_pp:>2}"
+            f", sum: {time_startup+time_main+time_pp:>4}"
+        )
+
+
+if __name__ == "__main__":
+    measure()
---
+++ benchmarks/rust/unchecked_arith/list_sum-extracted/src/main.rs
@@ -0,0 +1,315 @@
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+use std::marker::PhantomData;
+
+fn __nat_succ(x: u64) -> u64 {
+  x.checked_add(1).unwrap()
+}
+
+macro_rules! __nat_elim {
+    ($zcase:expr, $pred:ident, $scase:expr, $val:expr) => {
+        { let v = $val;
+        if v == 0 { $zcase } else { let $pred = v - 1; $scase } }
+    }
+}
+
+macro_rules! __andb { ($b1:expr, $b2:expr) => { $b1 && $b2 } }
+macro_rules! __orb { ($b1:expr, $b2:expr) => { $b1 || $b2 } }
+
+fn __pos_onebit(x: u64) -> u64 {
+  x.checked_mul(2).unwrap() + 1
+}
+
+fn __pos_zerobit(x: u64) -> u64 {
+  x.checked_mul(2).unwrap()
+}
+
+macro_rules! __pos_elim {
+    ($p:ident, $onebcase:expr, $p2:ident, $zerobcase:expr, $onecase:expr, $val:expr) => {
+        {
+            let n = $val;
+            if n == 1 {
+                $onecase
+            } else if (n & 1) == 0 {
+                let $p2 = n >> 1;
+                $zerobcase
+            } else {
+                let $p = n >> 1;
+                $onebcase
+            }
+        }
+    }
+}
+
+fn __Z_frompos(z: u64) -> i64 {
+  use std::convert::TryFrom;
+
+  i64::try_from(z).unwrap()
+}
+
+fn __Z_fromneg(z : u64) -> i64 {
+  use std::convert::TryFrom;
+
+  i64::try_from(z).unwrap().checked_neg().unwrap()
+}
+
+macro_rules! __Z_elim {
+    ($zero_case:expr, $p:ident, $pos_case:expr, $p2:ident, $neg_case:expr, $val:expr) => {
+        {
+            let n = $val;
+            if n == 0 {
+                $zero_case
+            } else if n < 0 {
+                let $p2 = n.unsigned_abs();
+                $neg_case
+            } else {
+                let $p = n as u64;
+                $pos_case
+            }
+        }
+    }
+}
+
+fn __N_frompos(z: u64) -> u64 {
+  z
+}
+
+macro_rules! __N_elim {
+    ($zero_case:expr, $p:ident, $pos_case:expr, $val:expr) => {
+        { let $p = $val; if $p == 0 { $zero_case } else { $pos_case } }
+    }
+}
+
+type __pair<A, B> = (A, B);
+
+macro_rules! __pair_elim {
+    ($fst:ident, $snd:ident, $body:expr, $p:expr) => {
+        { let ($fst, $snd) = $p; $body }
+    }
+}
+
+fn __mk_pair<A: Copy, B: Copy>(a: A, b: B) -> __pair<A, B> { (a, b) }
+
+fn hint_app<TArg, TRet>(f: &dyn Fn(TArg) -> TRet) -> &dyn Fn(TArg) -> TRet {
+  f
+}
+
+#[derive(Debug, Clone)]
+pub enum Coq_Init_Datatypes_list<'a, A> {
+  nil(PhantomData<&'a A>),
+  cons(PhantomData<&'a A>, A, &'a Coq_Init_Datatypes_list<'a, A>)
+}
+
+struct Program {
+  __alloc: bumpalo::Bump,
+}
+
+impl<'a> Program {
+fn new() -> Self {
+  Program {
+    __alloc: bumpalo::Bump::new(),
+  }
+}
+
+fn alloc<T>(&'a self, t: T) -> &'a T {
+  self.__alloc.alloc(t)
+}
+
+fn closure<TArg, TRet>(&'a self, F: impl Fn(TArg) -> TRet + 'a) -> &'a dyn Fn(TArg) -> TRet {
+  self.__alloc.alloc(F)
+}
+
+fn Coq_Lists_List_fold_left<A: Copy, B: Copy>(&'a self, f: &'a dyn Fn(A) -> &'a dyn Fn(B) -> A, l: &'a Coq_Init_Datatypes_list<'a, B>, a0: A) -> A {
+  match l {
+    &Coq_Init_Datatypes_list::nil(_) => {
+      a0
+    },
+    &Coq_Init_Datatypes_list::cons(_, b, t) => {
+      self.Coq_Lists_List_fold_left(
+        f,
+        t,
+        hint_app(hint_app(f)(a0))(b))
+    },
+  }
+}
+fn Coq_Lists_List_fold_left__curried<A: Copy, B: Copy>(&'a self) -> &'a dyn Fn(&'a dyn Fn(A) -> &'a dyn Fn(B) -> A) -> &'a dyn Fn(&'a Coq_Init_Datatypes_list<'a, B>) -> &'a dyn Fn(A) -> A {
+  self.closure(move |f| {
+    self.closure(move |l| {
+      self.closure(move |a0| {
+        self.Coq_Lists_List_fold_left(
+          f,
+          l,
+          a0)
+      })
+    })
+  })
+}
+
+fn Coq_Init_Nat_add(&'a self, n: u64, m: u64) -> u64 {
+  __nat_elim!(
+    {
+      m
+    },
+    p, {
+      __nat_succ(
+        self.Coq_Init_Nat_add(
+          p,
+          m))
+    },
+    n)
+}
+fn Coq_Init_Nat_add__curried(&'a self) -> &'a dyn Fn(u64) -> &'a dyn Fn(u64) -> u64 {
+  self.closure(move |n| {
+    self.closure(move |m| {
+      self.Coq_Init_Nat_add(
+        n,
+        m)
+    })
+  })
+}
+
+fn Coq_Lists_List_repeat<A: Copy>(&'a self, x: A, n: u64) -> &'a Coq_Init_Datatypes_list<'a, A> {
+  __nat_elim!(
+    {
+      self.alloc(
+        Coq_Init_Datatypes_list::nil(
+          PhantomData))
+    },
+    k, {
+      self.alloc(
+        Coq_Init_Datatypes_list::cons(
+          PhantomData,
+          x,
+          self.Coq_Lists_List_repeat(
+            x,
+            k)))
+    },
+    n)
+}
+fn Coq_Lists_List_repeat__curried<A: Copy>(&'a self) -> &'a dyn Fn(A) -> &'a dyn Fn(u64) -> &'a Coq_Init_Datatypes_list<'a, A> {
+  self.closure(move |x| {
+    self.closure(move |n| {
+      self.Coq_Lists_List_repeat(
+        x,
+        n)
+    })
+  })
+}
+
+fn CertiCoq_Benchmarks_tests_list_sum(&'a self) -> u64 {
+  self.Coq_Lists_List_fold_left(
+    self.Coq_Init_Nat_add__curried(),
+    self.Coq_Lists_List_repeat(
+      __nat_succ(
+        0),
+      __nat_succ(
+        __nat_succ(
+          __nat_succ(
+            __nat_succ(
+              __nat_succ(
+                __nat_succ(
+                  __nat_succ(
+                    __nat_succ(
+                      __nat_succ(
+                        __nat_succ(
+                          __nat_succ(
+                            __nat_succ(
+                              __nat_succ(
+                                __nat_succ(
+                                  __nat_succ(
+                                    __nat_succ(
+                                      __nat_succ(
+                                        __nat_succ(
+                                          __nat_succ(
+                                            __nat_succ(
+                                              __nat_succ(
+                                                __nat_succ(
+                                                  __nat_succ(
+                                                    __nat_succ(
+                                                      __nat_succ(
+                                                        __nat_succ(
+                                                          __nat_succ(
+                                                            __nat_succ(
+                                                              __nat_succ(
+                                                                __nat_succ(
+                                                                  __nat_succ(
+                                                                    __nat_succ(
+                                                                      __nat_succ(
+                                                                        __nat_succ(
+                                                                          __nat_succ(
+                                                                            __nat_succ(
+                                                                              __nat_succ(
+                                                                                __nat_succ(
+                                                                                  __nat_succ(
+                                                                                    __nat_succ(
+                                                                                      __nat_succ(
+                                                                                        __nat_succ(
+                                                                                          __nat_succ(
+                                                                                            __nat_succ(
+                                                                                              __nat_succ(
+                                                                                                __nat_succ(
+                                                                                                  __nat_succ(
+                                                                                                    __nat_succ(
+                                                                                                      __nat_succ(
+                                                                                                        __nat_succ(
+                                                                                                          __nat_succ(
+                                                                                                            __nat_succ(
+                                                                                                              __nat_succ(
+                                                                                                                __nat_succ(
+                                                                                                                  __nat_succ(
+                                                                                                                    __nat_succ(
+                                                                                                                      __nat_succ(
+                                                                                                                        __nat_succ(
+                                                                                                                          __nat_succ(
+                                                                                                                            __nat_succ(
+                                                                                                                              __nat_succ(
+                                                                                                                                __nat_succ(
+                                                                                                                                  __nat_succ(
+                                                                                                                                    __nat_succ(
+                                                                                                                                      __nat_succ(
+                                                                                                                                        __nat_succ(
+                                                                                                                                          __nat_succ(
+                                                                                                                                            __nat_succ(
+                                                                                                                                              __nat_succ(
+                                                                                                                                                __nat_succ(
+                                                                                                                                                  __nat_succ(
+                                                                                                                                                    __nat_succ(
+                                                                                                                                                      __nat_succ(
+                                                                                                                                                        __nat_succ(
+                                                                                                                                                          __nat_succ(
+                                                                                                                                                            __nat_succ(
+                                                                                                                                                              __nat_succ(
+                                                                                                                                                                __nat_succ(
+                                                                                                                                                                  __nat_succ(
+                                                                                                                                                                    __nat_succ(
+                                                                                                                                                                      __nat_succ(
+                                                                                                                                                                        __nat_succ(
+                                                                                                                                                                          __nat_succ(
+                                                                                                                                                                            __nat_succ(
+                                                                                                                                                                              __nat_succ(
+                                                                                                                                                                                __nat_succ(
+                                                                                                                                                                                  __nat_succ(
+                                                                                                                                                                                    __nat_succ(
+                                                                                                                                                                                      __nat_succ(
+                                                                                                                                                                                        __nat_succ(
+                                                                                                                                                                                          __nat_succ(
+                                                                                                                                                                                            __nat_succ(
+                                                                                                                                                                                              __nat_succ(
+                                                                                                                                                                                                __nat_succ(
+                                                                                                                                                                                                  __nat_succ(
+                                                                                                                                                                                                    __nat_succ(
+                                                                                                                                                                                                      __nat_succ(
+                                                                                                                                                                                                        __nat_succ(
+                                                                                                                                                                                                          __nat_succ(
+                                                                                                                                                                                                            __nat_succ(
+                                                                                                                                                                                                              0))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))),
+    0)
+}
+}
+pub fn main() {
+  Program::new().CertiCoq_Benchmarks_tests_list_sum();
+}
